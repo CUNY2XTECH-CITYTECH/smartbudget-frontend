8b90eb62d08ef6e18e6479d807a5d558ffcbaba0
India Barker <india.barker@mail.citytech.cuny.edu>
2025-08-07 14:05:52 -0400

    New changes

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,9 @@
 import "./dashboard.css";
-import logo from "./logo.png";
 import { Link } from "react-router-dom";
 
 const Dashboard = () => (
   <div className="dashboard-container">
-    <header className="home-header dashboard-header">
+    {/* <header className="home-header dashboard-header">
       <div className="left-section">
         <img src={logo} alt="SmartBudget Logo" className="logo" />
         <span className="slogan">Your Money, Organized.</span>
@@ -13,7 +12,7 @@ const Dashboard = () => (
         <Link to="/signup" className="btn btn-signup">Sign up</Link>
         <Link to="/login" className="btn btn-login">Log in</Link>
       </div>
-    </header>
+    </header> */}
 
     <div className="dashboard-main">
       <aside className="dashboard-sidebar">
