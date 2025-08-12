e99c7dd0b9dd8472b211a09748976e3487e04f23
India Barker <india.barker@mail.citytech.cuny.edu>
2025-08-12 14:01:42 -0400

    New changes

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,9 @@
 function Footer() {
     return (
         <footer class = "home-footer">
-            © 2025 SmartBudget. All rights reserved. | <a href="mailto:support@smartbudget.com">Contact Us</a>
+            © 2025 SmartBudget. All rights reserved. | 
+            <a  className="contact"
+            href="mailto:support@smartbudget.com">Contact Us</a>
         </footer>
     )
 }
